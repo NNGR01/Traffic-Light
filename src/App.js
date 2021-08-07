9f835d795ef6867967974cbb99ab6422618719ae
Nicolas Guzman Rivera <NNGR01@MacBook-Pro-de-Nicolas.local>
2021-08-06 22:17:19 -0400

    traffic-light

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  
+
+  const [ rojo, setRojo ] = useState("");
+  const [ amarillo, setAmarillo ] = useState("");
+  const [ verde, setVerde ] = useState("");
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+     <h1>traffic light
+     </h1>
+     <div className="Semaforo">
+       <div className={"rojo" + rojo } onClick={() => (
+       setRojo("selected"),
+       setAmarillo(""),
+       setVerde("")
+     
+       )}></div>
+         <div className={"amarillo" + amarillo } onClick={() => (
+       setRojo(""),
+       setAmarillo("selected"),
+       setVerde("")
+       )}></div>
+        <div className={"verde" + verde } onClick={() => (
+       setRojo(""),
+       setAmarillo(""),
+       setVerde("selected")
+       )}></div>
+     </div>
+  <div className="poste" onClick={() => (
+    setRojo(""),
+    setAmarillo(""),
+    setVerde("")
+  )}></div>
       </header>
+    
     </div>
   );
 }
